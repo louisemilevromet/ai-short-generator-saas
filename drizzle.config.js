0eb40cff4328ed153e6019107451c595251c30c5
Louis-Émile Vromet <levromet5@gmail.com>
2024-09-29 15:06:12 -0400

    initial commit

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+  schema: "./configs/schema.js",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: "postgresql://ai-short-generator-saas_owner:YXNZgb7qn3Fx@ep-curly-darkness-a5howp5c.us-east-2.aws.neon.tech/ai-short-generator-saas?sslmode=require",
+  },
+};
